664ba1d72ede7785c8d1f554dfc65cb861ae0dce
Lucas-G13 <lucasscarpinelli@hotmail.com>
2020-05-09 11:47:40 -0300

    First commit

---
+++ src/main/kotlin/work/lucasst/sip/SIPManager.kt
@@ -0,0 +1,12 @@
+package work.lucasst.sip
+
+
+
+class SIPManager(address: String, port: Int, username: String, password: String) {
+
+
+
+
+
+
+}
---
+++ src/test/kotlin/test.kt
@@ -0,0 +1,12 @@
+import work.lucasst.sip.SIPManager
+
+fun main(){
+
+
+    var manager = SIPManager("45.32.106.183", 2080, "1794859", "")
+
+
+
+
+
+}
